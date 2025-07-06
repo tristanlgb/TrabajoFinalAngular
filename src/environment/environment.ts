e171ef4ea7eedf90bccfbddd693e18865d442a51
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2025-07-06 03:00:43 -0300

    Add files via upload

---
+++ src/environment/environment.ts
@@ -0,0 +1,8 @@
+export const env ={
+  apiURL: 'https://64d526c5b592423e46953244.mockapi.io/',
+  authURL: 'https://64d526c5b592423e46953244.mockapi.io/',
+
+}
+export const env2 = {
+  apiURL: 'https://64d526c5b592423e46953244.mockapi.io/'
+}
